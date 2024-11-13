5242b2965036002883b5a839ec212d8d1c877e67
Lulwa Alkanderi <l.alkindri@gmail.com>
2024-11-13 20:40:47 +0300

    done

---
+++ src/Components/PetItem.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+function PetItem({ pet }) {
+  const [pic, setpic] = useState(pet.image);
+  const handleClick = () => {
+    if (pic === pet.image){
+      setpic(pet.image2);
+    }else {
+      setpic(pet.image);
+    };
+  } 
+  return (
+    <div className="col-lg-4 col-md-8 col-sm-10">
+      <div className="single-doctor">
+        <img className="image" alt={pet.name} src={pic} />
+        <div className="content">
+          <h3>{pet.name}</h3>
+          <button onClick = {handleClick} type="button" className="btn btn-info">
+            Pet
+          </button>
+          <button type="button" className="btn btn-info  m-2">
+            Adopt
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PetItem;
---
+++ src/Components/PetsList.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import pets from "../petsData";
+import PetItem from "./PetItem";
+
+function PetsList() {
+  const [query, setQuery] = useState("");
+  const [type, setType] = useState("")
+  const filterName = pets.filter((pet) => pet.name.toLocaleLowerCase().includes(query.toLocaleLowerCase()))
+  const filterType = filterName.filter((pets) => pets.type.includes(type))
+  const petList = filterType.map((pet) => <PetItem pet={pet} key={pet.id} />);
+  
+
+  return (
+    <section id="doctors" className="doctor-section pt-140">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-xxl-5 col-xl-6 col-lg-7">
+            <div className="section-title text-center mb-30">
+              <h1 className="mb-25 wow fadeInUp" data-wow-delay=".2s">
+                Fur-ends
+              </h1>
+              <div className="input-group rounded">
+                <input onChange={(event)=>setQuery(event.target.value)}
+                  type="search"
+                  className="form-control rounded"
+                  placeholder="Search"
+                  aria-label="Search"
+                  aria-describedby="search-addon"
+                />
+              </div>
+              <br />
+              Type:
+              <select onChange={(event) => setType(event.target.value)} className="form-select" defaultValue={""}>
+                <option value="" >
+                  All
+                </option>
+                <option value="Cat">Cat</option>
+                <option value="Dog">Dog</option>
+                <option value="Rabbit">Rabbit</option>
+              </select>
+            </div>
+          </div>
+        </div>
+
+        <div className="row justify-content-center">{petList}</div>
+      </div>
+    </section>
+  );
+}
+
+export default PetsList;
